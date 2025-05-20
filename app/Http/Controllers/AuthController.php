c205967e09f7d42b25cbf33317464bdbb2b602a2
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-05-21 00:13:37 +0100

    login functionlity

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,28 @@ class AuthController extends Controller
         ]);
 
         $authService->register($request);
-
+        return response()->json(['success', 'user registered']);
     }
+
+    public function login(AuthService $authService, Request $request) {
+        $credentials = $request->validate([
+          'name' => 'required|string|min:3|max:20',
+          'email' => 'required|emal',
+          'password' => 'required|min:5|max:30',
+        ]);
+
+        $token = $authService->login(credentials->only('email', 'password'));
+
+        if(!token) {
+            return response()->json(['Failed', 'Invalid credentials'], 401);
+        } else {
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => "Bearer",
+            ]);
+        }
+    }
+
+
    
 }
