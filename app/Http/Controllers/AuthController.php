3e10b67f48cf757022f3564cd53f3501663021fb
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-05-20 23:02:05 +0100

    changed auth controller to use services from an authservice class now

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,46 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Services\AuthService;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        $user = $request->validate([
-         'name' => 'required|min:2|max:10',
-         'email' => 'required|email',
-         'password' => 'required|confirmed|min:5|max:15',
+
+    public function register(AuthSerice $authService, Request $request) {
+        $request->validate([
+          'name' => 'required|string|min:3|max:20',
+          'email' => 'required|email',
+          'password' => 'reuqied|min:5|max:30',
         ]);
 
-        User::create([
-         'name' => $user->name,
-         'email' => $user->email,
-         'password' => \bcrypt($user->password),
-        ]);
-    }
-
-    public function login(Request $request) {
-        $user = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required|confirmed|min:5|max:15',
-        ]);
-
-        $user = User::where('email', $request->email)->first();
-
-        if(!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'message' => 'Invalid credentials', 
-            ], 401);
-        }
-        $token = $user->createToken('api_token')->plainTextToken;
-
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            ]);
-        
-    }
-
-    public function logout(Request $request) {
+        $authService->register($request);
 
     }
+   
 }
